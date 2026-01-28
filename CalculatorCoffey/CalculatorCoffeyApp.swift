133a92aada8738607bc63dbc268269c1e345a942
shanejcoffey <shanejcoffey27@gmail.com>
2026-01-28 14:29:02 -0600

    Initial Commit

---
+++ CalculatorCoffey/CalculatorCoffeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCoffeyApp.swift
+//  CalculatorCoffey
+//
+//  Created by SHANE COFFEY on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCoffeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorCoffeyTests/CalculatorCoffeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCoffeyTests.swift
+//  CalculatorCoffeyTests
+//
+//  Created by SHANE COFFEY on 1/28/26.
+//
+
+import Testing
+@testable import CalculatorCoffey
+
+struct CalculatorCoffeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
