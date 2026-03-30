d06af02245a26c96b9c947166167780aa887000c
shanejcoffey <shanejcoffey27@gmail.com>
2026-03-29 23:28:34 -0500

    Added marching squares for graphing, just a test implmentation. Works but super slow

---
+++ CalculatorCoffey/GraphView.swift
@@ -0,0 +1,18 @@
+//
+//  GraphView.swift
+//  CalculatorCoffey
+//
+//  Created by SHANE COFFEY on 3/23/26.
+//
+
+import SwiftUI
+
+struct GraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GraphView()
+}
---
+++ CalculatorCoffey/Graphing.swift
@@ -0,0 +1,8 @@
+//
+//  Graphing.swift
+//  CalculatorCoffey
+//
+//  Created by SHANE COFFEY on 3/23/26.
+//
+
+import Foundation
